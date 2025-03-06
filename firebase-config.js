8033f94b7a517eac2ef969e39e9568314dc7dee3
Burak Göl <burak22286@gmail.com>
2025-03-06 16:09:45 +0300

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -4,10 +4,8 @@ const firebaseConfig = {
     authDomain: "digitalwallet-45a8a.firebaseapp.com",
     projectId: "digitalwallet-45a8a",
     storageBucket: "digitalwallet-45a8a.firebasestorage.app",
-    messagingSenderId: "351579695434",
-    appId: "1:351579695434:web:f515645efa80ac6f8db9b3",
-    measurementId: "G-KM6M8QMFPD"
-  };
+    appId: "1:351579695434:web:f515645efa80ac6f8db9b3"
+};
 
 // Firebase'i başlat
 firebase.initializeApp(firebaseConfig); 
